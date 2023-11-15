d113a0a307540fa47c12cbc14b6dd3ad3ed8091e
Mikko <jokipuum@gmail.com>
2023-11-15 14:56:01 +0200

    add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   integrations: [
     tailwind({
       // Useful if you need to define and/or import your own custom `base.css`.
