b1c78273c8a4a559610454e3427197fa08e9ad72
Mikko <jokipuum@gmail.com>
2023-11-15 11:14:58 +0200

    add tailwind and a new font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+  ],
+});
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Geist'", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
