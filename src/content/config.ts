fa657029105ed3ba9a44654fe83764a6b855c5ec
Mikko <jokipuum@gmail.com>
2023-11-17 11:41:26 +0200

    build blog

---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+import { z, defineCollection } from "astro:content";
+
+export const collections = {
+  blog: defineCollection({
+    type: "content", // v2.5.0 and later
+    schema: z.object({
+      isDraft: z.boolean().default(true),
+      title: z
+        .string()
+        .min(1, { message: "Title must be at least 1 character long" }),
+      description: z
+        .string()
+        .min(1, { message: "Description must be at least 1 character long" }),
+      date: z.date(),
+      tags: z.array(z.string()),
+      image: z
+        .object({
+          src: z
+            .string()
+            .min(1, { message: "Image src must be at least 1 character long" }),
+          alt: z.string().min(1, {
+            message: "Image alt text must be at least 1 character long",
+          }),
+        })
+        .optional(),
+    }),
+  }),
+};
