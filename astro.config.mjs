a34adc0fec82f643678d93fdcf44d0eb57452136
Mikko <jokipuum@gmail.com>
2023-11-17 12:36:46 +0200

    add reading time to blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,9 @@ export default defineConfig({
     contentCollectionCache: true, // bypass the cache by passing `astro build --force`
   },
   prefetch: true,
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   integrations: [
     tailwind({
       // Useful if you need to define and/or import your own custom `base.css`.
